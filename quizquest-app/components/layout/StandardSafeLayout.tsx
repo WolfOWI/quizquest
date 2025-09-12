60e1ce00d9fcb764596d78aba1608d31d69b31f1
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-09-12 11:39:16 +0200

    Upgrade Expo to SDK V 54.0.0
    Upgrade from Expo SDK 53 to 54, including changing SafeAreaView component and versions of dependencies

--- quizquest-app/components/layout/StandardSafeLayout.tsx
+++ quizquest-app/components/layout/StandardSafeLayout.tsx
@@ -1,11 +1,11 @@
 import {
   View,
   Text,
-  SafeAreaView,
   KeyboardAvoidingView,
   ImageBackground,
   ImageSourcePropType,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
 
 const StandardSafeLayout = ({
