f634c23f802fa4d96a6ef0497186db7074583b99
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-09-30 09:58:21 +0200

    Setup Gemini AI
    Add gemini AI in firebase config

--- quizquest-app/config/firebaseConfig.js
+++ quizquest-app/config/firebaseConfig.js
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
+import { getAI, getGenerativeModel, GoogleAIBackend } from 'firebase/ai';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,10 +22,20 @@ const app = initializeApp(firebaseConfig);
 // Firestore Database
 const db = getFirestore(app);
 
+// Initialise the Gemini Developer API backend service
+const ai = getAI(app, { backend: new GoogleAIBackend() });
+
+const model = getGenerativeModel(ai, {
+  model: 'gemini-2.5-flash',
+  generationConfig: {
+    responseMimeType: 'application/json',
+  },
+});
+
 // Firebase Auth with React Native persistence
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-export { db, auth };
+export { db, auth, ai, model };
 export default app;
